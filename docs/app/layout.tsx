b10caeac54e56ac45a06d0cc337c593a29b668d6
Felipe Aragão <faragaop@gmail.com>
2024-02-23 20:24:54 -0300

    docs: fix logos not showing

--- docs/app/layout.tsx
+++ docs/app/layout.tsx
@@ -19,7 +19,7 @@ export default function RootLayout({
       <head>
         <link
           rel="icon"
-          href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>🫱🏻‍🫲🏽</text></svg>"
+          href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>🤝</text></svg>"
         />
       </head>
       <body className="bg-background text-default m-auto flex w-full flex-col gap-20 selection:bg-fuchsia-300 selection:text-fuchsia-900 ">

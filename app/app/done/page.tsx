fe1294cb82acb96956aa8cb9b9375431839da2d6
Felipe Aragão <faragaop@gmail.com>
2024-02-27 14:39:40 -0300

    app: fix done page

--- app/app/done/page.tsx
+++ app/app/done/page.tsx
@@ -1,9 +1,17 @@
 "use client";
 
 import { useSearchParams } from "next/navigation";
-import { useEffect } from "react";
+import { Suspense, useEffect } from "react";
 
 export default function Page() {
+  return (
+    <Suspense>
+      <InnerPage />
+    </Suspense>
+  );
+}
+
+function InnerPage() {
   const searchParams = useSearchParams();
 
   useEffect(() => {
